4a69b604b8989ed0d52ee9a87025ad7fa32a73de
pranavtss <pranavsubburaj@gmail.com>
2026-03-14 16:03:15 +0530

    backup

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(helmet({
 }))
 app.use(morgan("combined"))
 app.use(cors())
-app.use(express.json())
+app.use(express.json({ limit: "10mb" }))
 const uploadsDir = path.join(__dirname, "uploads")
 if (!fs.existsSync(uploadsDir)) {
   fs.mkdirSync(uploadsDir, { recursive: true })
